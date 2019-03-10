d3455db8fbf9dd72545003e4ceae184b881ca5dc
hieupham9809 <hieupham9809@gmail.com>
2019-03-10 18:29:05 +0700

    hashpw

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  data.saveInfo()
   
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
