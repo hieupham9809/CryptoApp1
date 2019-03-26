d67df03876fa4a41f2f6dedca5890e6c1688b74d
NguyenNhuY <thachdi88@gmail.com>
2019-03-26 22:18:16 +0700

    Đã fix no des_path

--- dist/template-app/call_encrypt.js
+++ dist/template-app/call_encrypt.js
@@ -1,38 +1,47 @@
 function encrypt(){
-        cute = document.getElementById("desPath").files[0].path;
+    debugger;
+        des_path = document.getElementById("desPath").value; 
+        if (des_path == "") {
+            cute = "";
+        }
+        else {
+            cute = document.getElementById("desPath").files[0].path;
+        }
         cute1 = document.getElementById("myFile").files[0].path;
         cute2 = document.getElementById("keyFile").files[0].path;
-          // window.Safe = Safe;
-          // let s=new Safe();
-     
-
-        // alert (document.getElementById('myFile').files[0].path);
-        // result = (Safe.encrypt_aes256cbc('./File  (copy).txt','./KeyFile.txt','./Test'));
-        // Safe.encrypt_aes256cbc('./File  (copy).txt','./key.txt','./Test');
-        // Safe.encrypt_aes256cbc(cute1,cute2,cute);
-        // alert (result);
-
-        // result = (Safe.encrypt_blowfish(cute1,cute2,cute));
-        // alert (result);
-
-
+      
         var e = document.getElementById("hashtype");
         var hashtype = e.options[e.selectedIndex].value;
 
         if (hashtype == "AES"){
-            result = (Safe.encrypt_aes256cbc(cute1,cute2,cute));
+            if (cute == ""){
+                result = (Safe.encrypt_aes256cbc(cute1,cute2));
+            }
+            else{
+                result = (Safe.encrypt_aes256cbc(cute1,cute2,cute));
+            }
             if (result){
                 alert ("Encrypt successfully!")
             }
         }
-        else if (hashtype == "RSA") {
-            result = (Safe.encrypt_rsa(cute1,cute2,cute));
+        else if (hashtype == "RSA") {          
+            if (cute == ""){
+                result = (Safe.encrypt_rsa(cute1,cute2));
+            }
+            else{
+                result = (Safe.encrypt_rsa(cute1,cute2,cute));
+            }
             if (result){
                 alert ("Encrypt successfully!")
             }
         }
-        else{
-            result = (Safe.encrypt_blowfish(cute1,cute2,cute));
+        else{            
+            if (cute == ""){
+                result = (Safe.encrypt_blowfish(cute1,cute2));
+            }
+            else{
+                result = (Safe.encrypt_blowfish(cute1,cute2,cute));
+            }
             if (result){
                 alert ("Encrypt successfully!")
             }
